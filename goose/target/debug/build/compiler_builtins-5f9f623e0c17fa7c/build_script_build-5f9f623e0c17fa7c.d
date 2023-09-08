3ed1780fdf9fd058f5fd6e5e3bdc2a22401032f1
gerrit <overlorde159@gmail.com>
2023-09-08 12:18:35 -0400

    now it can mostly build

---
+++ goose/target/debug/build/compiler_builtins-5f9f623e0c17fa7c/build_script_build-5f9f623e0c17fa7c.d
@@ -0,0 +1,5 @@
+/home/gerrit/projects/os_dev/goose/target/debug/build/compiler_builtins-5f9f623e0c17fa7c/build_script_build-5f9f623e0c17fa7c: /home/gerrit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.100/build.rs
+
+/home/gerrit/projects/os_dev/goose/target/debug/build/compiler_builtins-5f9f623e0c17fa7c/build_script_build-5f9f623e0c17fa7c.d: /home/gerrit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.100/build.rs
+
+/home/gerrit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.100/build.rs:
