65ad4bf378eace9335f9183e1389a3a28c76ade6
gerrit <overlorde159@gmail.com>
2023-09-12 13:26:42 -0400

    hex -> string works!

--- gooseboots/print_hex.asm
+++ gooseboots/print_hex.asm
@@ -4,6 +4,7 @@
 
 print_hex:
   pusha
+  mov ah, 0x0e
   mov cx, 0
 
   hex_loop:
@@ -12,14 +13,14 @@ print_hex:
 
     mov ax, dx
     and ax, 0x000F         ; masks everything but the first character (or last, depending on perspective)
+    add al, 0x30         
     cmp al, 0x39           ; looking at the bottom two hex values, if it's  <= it's a number, otherwise it's a character
     jle hex_mv_to_bx
     add al, 0x7            ; to get our ascii 'A' through 'F', this works because it shifts
     hex_mv_to_bx:
-      add al, 0x30         ; this works because the hex range is at least 30 below the actual ascii values, but the capitals are offset by 7 from the numbers in ascii
     mov bx, hex_string + 5 ; base address of string + string len
     sub bx, cx             ; subtract iterator value
-    inc cx                 ; if it fucks up, this is the issue
+    add cx, 1              ; if it fucks up, this is the issue
     mov [bx], al           ; actually assign the value into our string
     ror dx, 4              ; rotate right by 4 bits, like a carousel, so like 0x1234 -> 0x4123 (only does this for hex because 4 bits because 2^4)
     jmp hex_loop
