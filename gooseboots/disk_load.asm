d85c8811cdb43eaff19d7d025148674a6b8fc961
gerrit <overlorde159@gmail.com>
2023-09-14 09:33:12 -0400

    we can now load from disk!!!

---
+++ gooseboots/disk_load.asm
@@ -0,0 +1,24 @@
+disk_load:
+  push dx           ; stores how many sectors we want read for later
+
+  mov ah, 0x02      ; BIOS Read Sector function
+  mov al, dh        ; read a dh number of sectors
+  mov ch, 0x00      ; select cylinder 0
+  mov dh, 0x00      ; select head 0
+  mov cl, 0x02      ; start reading from second sector (after boot)
+
+  int 0x13          ; friendly neightborhood bios interrupt
+
+  jc disk_error
+
+  pop dx
+  cmp dh, al        ; if al (sectors read) != dh (sectors expected)
+  jne disk_error    ; something's gone wrong
+  ret
+
+disk_error:
+  mov bx, DISK_ERROR_MSG
+  call print_string
+  jmp $
+
+DISK_ERROR_MSG db "Disk read error!", 0
--- gooseboots/gooseboots.asm
+++ gooseboots/gooseboots.asm
@@ -1,14 +1,20 @@
-;this is because our labels don't account for correct location, this is our proper offset
+; for data sector, allows for proper offset
 [org 0x7c00]
 
-; set up stack just a little below the bootloader
-mov bp, 0x8000
-mov sp, bp ; stack pointer and base pointer
+mov [BOOT_DRIVE], dl ; stores boot drive in dl, let's remember this for later
 
-mov bx, String
+mov bp, 0x8000 ; set up stack just a little below the bootloader
+mov sp, bp     ; stack pointer and base pointer
+
+mov bx, String1
 call print_string
 
-mov dx, 0xA99A
+mov bx, 0x9000           ; load 5 sectors to 0x0000(ES):0x9000(BX)
+mov dh, 5               ; from boot disk (dl)
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+mov dx, [0x9000 + 512]
 call print_hex
 
 end:
@@ -16,9 +22,16 @@ end:
 
 %include "print_string.asm"
 %include "print_hex.asm"
+%include "disk_load.asm"
 
-String:
-  db "Hello, world!", 0
+String1:
+  db "Attempting a load from disk!    ", 0
+
+; global variable
+BOOT_DRIVE: db 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+times 256 dw 0
+times 256 dw 0xface
