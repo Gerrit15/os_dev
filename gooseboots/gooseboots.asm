b65a330c7b26846b7c02123319c2ebb6d604895a
gerrit <overlorde159@gmail.com>
2023-09-15 10:44:06 -0400

    formatting

--- gooseboots/gooseboots.asm
+++ gooseboots/gooseboots.asm
@@ -29,31 +29,3 @@ MSG_PROT_MODE:
 
 times 510-($-$$) db 0
 dw 0xaa55
-
-;mov bx, MSG_REAL_MODE
-;call print_string_rm
-
-;call switch_to_pm
-
-;jmp $
-
-;%include "print_string.asm"
-;%include "print_string_rm.asm"
-;%include "disk_load.asm"
-;%include "gdt.asm"
-;%include "switch_to_pm.asm"
-
-;[bits 32]
-
-;BEGIN_PM:
-;  mov ebx, MSG_PROT_MODE
-;  call print_string_pm
-
-;end:
-;  jmp $
-
-;MSG_REAL_MODE: db "Started in 16-bit Real Mode", 0
-;MSG_PROT_MODE: db "Successfullly landed in 32-bit Protected mode", 0
-
-;times 510-($-$$) db 0
-;dw 0xaa55
